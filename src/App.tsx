08740dd22e44d3aeec12d9a3c9d771f725e248e9
v1xingyue <qixingyue@gmail.com>
2023-06-22 06:48:13 +0800

    add wallet connect

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 import { Button } from "@mui/material";
+import { WalletConnector } from "@aptos-labs/wallet-adapter-mui-design";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,12 +9,7 @@ function App() {
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <WalletConnector />
       </div>
       <h1>Vite + React + + Typescript + MUI 5</h1>
       <Button color="secondary">Secondary</Button>
