73bdeb93fc2f9a130332b2fee6a6cb064a252619
v1xingyue <qixingyue@gmail.com>
2023-06-27 09:55:35 +0800

    简单更新UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
-import { Button } from "@mui/material";
+import { Button, TextField, Stack } from "@mui/material";
+
 import { WalletConnector } from "@aptos-labs/wallet-adapter-mui-design";
 import { useAutoConnect } from "./components/AutoConnectProvider";
-import Stack from "@mui/material/Stack";
 import { useWallet } from "@aptos-labs/wallet-adapter-react";
 import { Provider, Network } from "aptos";
 
@@ -10,11 +10,13 @@ import { MyThemeSwitch } from "./components/MyThemeContext";
 import { DAPP_ADDRESS, PackageLink } from "./utils/const";
 
 function App() {
-  const [counter, updateCounter] = useState<any>({});
-  const [count, setCount] = useState(1000);
+  const [counter, updateCounter] = useState<{
+    value: number;
+    description: string;
+    allow: string;
+  }>({ value: 0, description: "", allow: "" });
   const { autoConnect, setAutoConnect } = useAutoConnect();
   const { network, account, wallet, signAndSubmitTransaction } = useWallet();
-  const [address, updateAddress] = useState("");
   const [coinInfo, updateAccountCoin] = useState({});
 
   let provider = new Provider(Network.MAINNET);
@@ -43,17 +45,17 @@ function App() {
           });
 
         if (counter) {
-          updateCounter(counter);
+          console.log(`counter reuslt : ${JSON.stringify(counter)}`);
+          const data = counter.data as any;
+          updateCounter({
+            value: data.value,
+            description: data.description,
+            allow: data.allow,
+          });
         }
       }
     };
     asyncTask();
-  }, [account]);
-
-  useEffect(() => {
-    if (account) {
-      updateAddress(account.address);
-    }
   }, [account]);
 
   const createCounter = async () => {
@@ -61,85 +63,102 @@ function App() {
       type: "entry_function_payload",
       function: `${DAPP_ADDRESS}::mycounter::make_counter`,
       type_arguments: [],
-      arguments: [
-        "0x3ee0661c3e99c34d502daa36a2bd12b6b3bd52b6762c2f071cddd6a187b17309",
-        0,
-        "hello default message",
-      ],
+      arguments: [counter.allow, counter.value, counter.description],
     };
     await signAndSubmitTransaction(params);
   };
 
-  return (
-    <div>
-      <p>
+  const MyHeader = () => {
+    return (
+      <div>
         <WalletConnector />
         <MyThemeSwitch />
-      </p>
-      <p>
-        Explorer:{" "}
-        <a target="_blank" rel="noreferrer" href={PackageLink()}>
-          {DAPP_ADDRESS}
-        </a>
-      </p>
-
-      <p>{JSON.stringify(wallet)}</p>
-      <p>{JSON.stringify(network)}</p>
-      <p>{JSON.stringify(account)}</p>
-      <p>{JSON.stringify(coinInfo)}</p>
-      <p>{address}</p>
-
-      <p>
-        {counter.data == null ? (
+        <span
+          style={{
+            float: "right",
+            marginRight: "1.5rem",
+          }}
+        >
+          Explorer:{" "}
+          <a target="_blank" rel="noreferrer" href={PackageLink()}>
+            {DAPP_ADDRESS}
+          </a>
+        </span>
+        {autoConnect ? null : (
           <Button
+            variant="contained"
+            color="success"
+            onClick={() => {
+              setAutoConnect(true);
+            }}
+          >
+            Set autoConnect
+          </Button>
+        )}
+      </div>
+    );
+  };
+
+  const BasicWallet = () => {
+    return (
+      <div style={{ marginTop: "2rem" }}>
+        <p>{JSON.stringify(wallet)}</p>
+        <p>{JSON.stringify(network)}</p>
+        <p>{JSON.stringify(account)}</p>
+        <p>{JSON.stringify(coinInfo)}</p>
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      <MyHeader />
+      <BasicWallet />
+
+      <Stack direction="column" spacing={3}>
+        <div>
+          <TextField
+            label="Counter value"
+            variant="standard"
+            type="number"
+            value={counter.value}
+            sx={{
+              width: "50%",
+            }}
+            onChange={(e) =>
+              updateCounter({ ...counter, value: parseInt(e.target.value, 10) })
+            }
+          />
+        </div>
+        <div>
+          <TextField
+            label="Allow update address"
+            variant="standard"
+            value={counter.allow}
+            sx={{
+              width: "50%",
+            }}
+            onChange={(e) =>
+              updateCounter({ ...counter, allow: e.target.value })
+            }
+          />
+        </div>
+        <div>Description: {counter.description}</div>
+        <div>
+          <Button
+            sx={{
+              marginTop: "1rem",
+            }}
             variant="contained"
             color="info"
             onClick={() => {
-              createCounter();
+              counter.value === 0 ? createCounter() : createCounter();
             }}
           >
-            Create a counter
+            {counter.value === 0 ? "create counter" : "update counter"}
           </Button>
-        ) : (
-          <>
-            counter holder :{" "}
-            <b>
-              <pre>{JSON.stringify(counter, null, 2)}</pre>
-            </b>
-          </>
-        )}
-      </p>
-      {autoConnect ? null : (
-        <Button
-          variant="contained"
-          color="success"
-          onClick={() => {
-            setAutoConnect(true);
-          }}
-        >
-          Set autoConnect
-        </Button>
-      )}
-
-      <div className="card">
-        <p>Current count value is : {count} </p>
-        <Stack spacing={1} direction="row">
-          <Button
-            variant="contained"
-            color="success"
-            onClick={() => setCount((count) => count + 1)}
-          >
-            +
-          </Button>
-          <Button
-            color="success"
-            variant="contained"
-            onClick={() => setCount((count) => count - 1)}
-          >
-            -
-          </Button>
-        </Stack>
-      </div>
+        </div>
+      </Stack>
     </div>
   );
 }
