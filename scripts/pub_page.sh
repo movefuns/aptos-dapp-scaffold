3b133a7d01d1f1f84b663f0c04f321aa5876526b
v1xingyue <qixingyue@gmail.com>
2023-06-28 09:25:42 +0800

    publish pages 20230628090642

---
+++ scripts/pub_page.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "clean old pages"
+rm -rf docs/index.html docs/assets/ 
+echo "copy generated files"
+cp -R dist/index.html dist/assets docs/
+git add docs
+now=$(date +%Y%m%d%H%m%S)
+git commit -m "publish pages ${now} "
+git push origin main 
